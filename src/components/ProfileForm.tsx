8cc32c3c2971b1b7e1b987864c39750726d457ef
Jean Dresbach <jeanr.dresbach@gmail.com>
2024-04-17 00:50:09 -0300

    profile page and form created

---
+++ src/components/ProfileForm.tsx
@@ -0,0 +1,155 @@
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { Button, Grid, TextField, Typography } from "@mui/material"
+import { blueGrey } from "@mui/material/colors"
+
+import {
+  useAppSelector,
+  useAppDispatch,
+  toggleLoading,
+  openSnackbar,
+  removeUserData
+} from "../redux"
+import { updateUser } from "../services/api"
+import { ConfirmModal } from "./ConfirmModal"
+
+export function ProfileForm() {
+  const dispatch = useAppDispatch()
+  const user = useAppSelector((state) => state.user)
+  const session = useAppSelector((state) => state.session)
+
+  const navigate = useNavigate()
+
+  const [openConfirmModal, setOpenConfirmModal] = useState(false)
+  const [disabled, setDisabled] = useState(true)
+  const [profileFormData, setProfileFormData] = useState({
+    name: user?.name,
+    email: user?.email
+  })
+
+  useEffect(() => {
+    const handleCheckProfileInputs = () => {
+      if (
+        profileFormData.email !== user?.email ||
+        profileFormData.name !== user?.name
+      ) {
+        setDisabled(false)
+      } else {
+        setDisabled(true)
+      }
+    }
+
+    handleCheckProfileInputs()
+  }, [profileFormData.email, profileFormData.name, user?.email, user?.name])
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+
+    setProfileFormData((prev) => ({ ...prev, [name]: value }))
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    setOpenConfirmModal(true)
+  }
+
+  const handleConfirmSubmit = async () => {
+    dispatch(toggleLoading())
+
+    const result = await updateUser(
+      session?.csrfToken as string,
+      session?.userId as string,
+      profileFormData
+    )
+
+    dispatch(toggleLoading())
+
+    if (result.code === 401) {
+      dispatch(openSnackbar({ text: result.message, severity: "error" }))
+
+      navigate("/login")
+    } else if (result.code === 200) {
+      dispatch(removeUserData())
+      dispatch(openSnackbar({ text: result.message }))
+
+      setTimeout(() => navigate("/"), 2000)
+    }
+
+    setOpenConfirmModal(false)
+  }
+
+  return (
+    <>
+      <Grid container component="form" onSubmit={handleSubmit}>
+        <Grid
+          item
+          xs={12}
+          sm={2}
+          sx={{ display: "flex", alignItems: "center", mb: 3 }}
+        >
+          <Typography
+            htmlFor="email-profile-form"
+            component="label"
+            color={blueGrey[200]}
+          >
+            E-MAIL
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={10}
+          sx={{ display: "flex", alignItems: "center", mb: 3 }}
+        >
+          <TextField
+            id="email-profile-form"
+            name="email"
+            value={profileFormData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={2}
+          sx={{ display: "flex", alignItems: "center", mb: 3 }}
+        >
+          <Typography
+            htmlFor="name-profile-form"
+            component="label"
+            color={blueGrey[200]}
+          >
+            NOME
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={10}
+          sx={{ display: "flex", alignItems: "center", mb: 3 }}
+        >
+          <TextField
+            id="name-profile-form"
+            name="name"
+            value={profileFormData.name}
+            onChange={handleInputChange}
+            required
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <Button variant="contained" type="submit" disabled={disabled}>
+            Salvar Mudanças
+          </Button>
+        </Grid>
+      </Grid>
+      <ConfirmModal
+        isOpen={openConfirmModal}
+        setIsOpen={setOpenConfirmModal}
+        onConfirm={handleConfirmSubmit}
+      />
+    </>
+  )
+}
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,36 @@
-import { Box, Divider, Typography } from "@mui/material"
+import { Box, Divider, Typography, useTheme } from "@mui/material"
+
+import { ProfileForm } from "../components/ProfileForm"
 
 export function Profile() {
+  const theme = useTheme()
+
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", gap: 2, p: 3 }}>
-      <Typography variant="h5" component="h2" fontWeight={500}>
-        Profile
-      </Typography>
-      <Divider />
+    <Box
+      component="main"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        p: 3
+      }}
+    >
+      <Box
+        component="section"
+        sx={{
+          border: `1px solid ${theme.palette.primary.main}`,
+          borderRadius: 2,
+          p: 3
+        }}
+      >
+        <Typography variant="h5" component="h2" fontWeight={500}>
+          Perfil
+        </Typography>
+
+        <Divider sx={{ my: 3 }} />
+
+        <ProfileForm />
+      </Box>
     </Box>
   )
 }
