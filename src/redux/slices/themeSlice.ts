b7d4e38a808d6c63e2fb2c4365f6a63d7bfcf223
Jean Dresbach <jeanr.dresbach@gmail.com>
2024-04-17 00:52:52 -0300

    forgot to add

--- src/redux/slices/themeSlice.ts
+++ src/redux/slices/themeSlice.ts
@@ -3,7 +3,7 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit"
 const initialState: string = "light"
 
 export const themeSlice = createSlice({
-  name: "themes",
+  name: "theme",
   initialState,
   reducers: {
     setTheme: (_, action: PayloadAction<"light" | "dark">) => {
